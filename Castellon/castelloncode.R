7c1b24b482e48a6f704380a315471b61c1d4018e
Moradii <m2.moradi@yahoo.com>
2017-10-24 09:58:29 +0200

    plotting line code added

--- Castellon/castelloncode.R
+++ Castellon/castelloncode.R
@@ -4,3 +4,16 @@ out <- npdensity.lpp(X,sigma = 150,delta=3)
 proc.time()-a
 plot(out)
 plot(out,style = "w",adjust = 0.3)
+
+
+pdf("castellon.pdf")
+plot(X,main="",pch=20)
+dev.off()
+
+pdf("castellonw.pdf")
+plot(out,style = "w",main="",adjust = 0.3)
+dev.off()
+
+pdf("castellonp.pdf")
+plot(out,main="")
+dev.off()
